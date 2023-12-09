2fd5c0aca547cd823049c2838889e415885507cc
Jaime <jfcls@discente.ifpe.edu.br>
2023-12-09 15:56:31 -0300

    upt

--- src/main/java/br/com/ifpe/review/modelo/Lista/ListaService.java
+++ src/main/java/br/com/ifpe/review/modelo/Lista/ListaService.java
@@ -1,18 +1,12 @@
 package br.com.ifpe.review.modelo.Lista;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import br.com.ifpe.review.api.lista.ListaRequest;
-import br.com.ifpe.review.modelo.comentario.Comentario;
-
 import java.time.LocalDate;
 import java.util.List;
 
 import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ListaService {
@@ -22,13 +16,14 @@ public class ListaService {
 
     @Transactional
     public Lista save(Lista lista) {
+        lista.setHabilitado(Boolean.TRUE);
+        lista.setVersao(1L);
+        lista.setDataCriacao(LocalDate.now());
         return listaRepository.save(lista);
     }
 
     public Lista create(Lista lista) {
-        lista.setHabilitado(Boolean.TRUE);
-        lista.setVersao(1L);
-        lista.setDataCriacao(LocalDate.now());
+        lista.setHabilitado(Boolean.TRUE); // Adicione esta linha
         return listaRepository.save(lista);
     }
 
@@ -39,5 +34,4 @@ public class ListaService {
     public Lista findById(Long id) {
         return listaRepository.findById(id).orElse(null);
     }
-
 }
